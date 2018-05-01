c4a79a90f7f9ca0c5136e820d14853c6b2b1f9bf
fichtre78 <38886981+fichtre78@users.noreply.github.com>
2018-05-01 19:20:01 +0200

    Update ennemi.h

--- ennemi.h
+++ ennemi.h
@@ -3,18 +3,26 @@
 #include <Imagine/Graphics.h>
 using namespace Imagine;
 
-class ennemi
+#include "vector.h"
+#include "arme.h"
+
+class Ennemi
 {
     //Attributs
-
+    Vector m_pos;
     int m_hp;
     double m_speed;
     double m_size;
     bool m_reach_end;
     Imagine::NativeBitmap m_skin;
 
-
-
     //Méthodes
+    void recevoir_degat(Arme);
+    void avancer();
+    void death();
+};
+
+class Punaise : public Ennemi
+{
 
 };
